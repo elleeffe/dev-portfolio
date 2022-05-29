c3564c675149eb0ecf81dfc004b0e32437e0aa0e
Faenzi Lorenzo <faenzilorenzo@gmail.com>
2022-05-29 10:05:04 +0200

    Initial commit from gatsby

---
+++ gatsby-config.ts
@@ -0,0 +1,11 @@
+import type { GatsbyConfig } from "gatsby";
+
+const config: GatsbyConfig = {
+  siteMetadata: {
+    title: `dev-portfolio`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: ["gatsby-plugin-styled-components", "gatsby-plugin-react-helmet", "gatsby-plugin-sitemap"]
+};
+
+export default config;
